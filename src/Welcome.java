2bfd6581768af23d30b75cdf23035d6fbcb752cc
Lekingna16 <23729131.ngan@student.iuh.edu.vn>
2024-08-15 22:02:30 +0700

    Frist commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main (String[] args) {
+        System.out.println("Hello Le Thi Kim Ngan");
+        System.out.println("Welcome to DHKTPM19B");
+    }
+}
